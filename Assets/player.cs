1a21d849aeb7de2e7bdf0799de12edeb58347285
Unknown <halea@oxy.edu>
2018-11-19 22:51:48 -0800

    Added max and minHeight
    Keeps player from continuously moving up or down - essentially setting a ceiling and ground for the game.

--- Assets/player.cs
+++ Assets/player.cs
@@ -7,16 +7,23 @@ public class player : MonoBehaviour {
     private Vector2 targetPos;
     public float Yincrement;
 
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+	void Update ()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
-            transform.position = targetPos;
-        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
-            transform.position = targetPos;
+
         }
 	}
 }
