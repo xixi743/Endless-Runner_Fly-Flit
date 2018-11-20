e9897e7f7ff82843d360e1ddd82eada907d77f68
Unknown <halea@oxy.edu>
2018-11-19 22:41:44 -0800

    Character and player script
    The initial form of the character (including fly animation) and the basic movement script for the character.

---
+++ Assets/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    private Vector2 targetPos;
+    public float Yincrement;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetPos;
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetPos;
+        }
+	}
+}
