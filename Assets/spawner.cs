a104299a7b1cc738eed79f8f67eb777b0bb2ad47
Unknown <halea@oxy.edu>
2018-11-20 01:40:09 -0800

    Added Spawner
    For some reason, I can't see the game objects spawned by the spawner... They do exist in the hierarchy and are visible when the game pauses, but not in game. hmm....

---
+++ Assets/spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+    public GameObject obstacle;
+
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+
+    private void Update()
+    {
+        if (timeBtwSpawn <= 0)
+        {
+            Instantiate(obstacle, transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+        } else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
