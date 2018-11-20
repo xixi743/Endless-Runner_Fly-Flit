7e5636473d10fc45e7dd239b3d4c1207202da733
Unknown <halea@oxy.edu>
2018-11-20 15:21:22 -0800

    Created Random Spawn Patterns
    Added the 3 possible obstacle spawn patterns for a game with 3 rows. Performance is suffering, likely from the polygon colliders I'm using.

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour {
 
-    public GameObject obstacle;
+    public GameObject[] obstaclePatterns;
 
     private float timeBtwSpawn;
     public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime = 0.65f;
 
     private void Update()
     {
         if (timeBtwSpawn <= 0)
         {
-            Instantiate(obstacle, transform.position, Quaternion.identity);
+            int rand = Random.Range(0, obstaclePatterns.Length);
+            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
             timeBtwSpawn = startTimeBtwSpawn;
+            if (startTimeBtwSpawn > minTime)
+            {
+                startTimeBtwSpawn -= decreaseTime;
+            }        
         } else
         {
             timeBtwSpawn -= Time.deltaTime;
---
+++ Assets/spawnpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnpoint : MonoBehaviour {
+
+    public GameObject obstacle;
+
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(obstacle, transform.position, Quaternion.identity);
+	}
+	
+
+}
